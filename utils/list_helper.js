ac710e5ccecc2fd1ef543c7e7b63a1c7578fda49
Davor <davor.komusanac@gmail.com>
2022-02-06 21:57:33 +0100

    first unit tests

---
+++ utils/list_helper.js
@@ -0,0 +1,70 @@
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  const reducer = (sumBlogsLikes, blog) => sumBlogsLikes + blog.likes;
+  return blogs.reduce(reducer, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  const reducer = (prev, next) => {
+    if (next.likes > prev.likes) {
+      return next;
+    } else {
+      return prev;
+    }
+  };
+  return blogs.reduce(reducer);
+};
+
+const mostBlogs = (blogs) => {
+  const reducer = (prev, next) => {
+    prev[next.author] = prev[next.author] ? prev[next.author] + 1 : 1;
+    return prev;
+  };
+  const mostBlogsObj = blogs.reduce(reducer, {});
+
+  let authorWithMostBlogs = {
+    author: "nameless",
+    blogs: 0,
+  };
+  const mostBlogsMap = new Map(Object.entries(mostBlogsObj));
+  mostBlogsMap.forEach((value, key) => {
+    if (value > authorWithMostBlogs.blogs) {
+      authorWithMostBlogs.author = key;
+      authorWithMostBlogs.blogs = value;
+    }
+  });
+  return authorWithMostBlogs;
+};
+
+const mostLikes = (blogs) => {
+  const reducer = (prev, next) => {
+    prev[next.author] = prev[next.author]
+      ? prev[next.author] + next.likes
+      : next.likes;
+    return prev;
+  };
+  const mostLikesObj = blogs.reduce(reducer, {});
+  let authorWithMostLikes = {
+    author: "nameless",
+    likes: 0,
+  };
+  const mostBlogsMap = new Map(Object.entries(mostLikesObj));
+  mostBlogsMap.forEach((value, key) => {
+    if (value > authorWithMostLikes.likes) {
+      authorWithMostLikes.author = key;
+      authorWithMostLikes.likes = value;
+    }
+  });
+  return authorWithMostLikes;
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
+};
